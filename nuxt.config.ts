21f271c51731b2bda9fd1de17f21d7a1db439f62
Rasool <rasoolol.77@gmail.com>
2025-12-06 13:56:47 +0330

    perf: install tailwind in project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ['~/assets/css/main.css'],
 
   modules: ['@nuxt/ui', '@nuxtjs/google-fonts', '@nuxt/image'],
 
